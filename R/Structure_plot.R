d7f75898e097dcf74cdb3d84bb64634636028619
soren <soren.johnson@selu.edu>
2021-11-11 21:15:41 +0000

    little change

--- R/Structure_plot.R
+++ R/Structure_plot.R
@@ -1,4 +1,4 @@
 #Create a structure/admixture plot that is ordered by inferred genetic cluster then longitude
 
 #Expected inputs: .Q file, text file with population data and GPS coordinates
-#Expected outputs: a structure barplot that is ordered first by inferred genetic cluster then by longitude.
+#Expected outputs: a structure bar plot that is ordered first by inferred genetic cluster then by longitude.
