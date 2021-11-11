6881fd8d82f8d703ef28cfea65a32583f432d408
soren <soren.johnson@selu.edu>
2021-11-11 21:06:45 +0000

    Added expected input and exput

--- R/AdmixtureMap.R
+++ R/AdmixtureMap.R
@@ -1,1 +1,4 @@
 #Create a map of the samples that have admixture pie charts
+
+#Expected inputs: csv or text file with the longitude and latitude of samples as well as population data, a .Q file from ADMIXTURE, size for the pie chart
+#Expected outputs: a map with every sample as a pie chart of the admxiture. The samples that are from the same location should have a combined piechart that is a larger size based off the number of samples. I also want to have the ability for the sample name to be displayed on the map
