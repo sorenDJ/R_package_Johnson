75e02fe0d90cbf5e6623507cbda7472d798ed92f
soren <soren.johnson@selu.edu>
2021-11-27 19:01:55 +0000

    Update

--- R/CleanUpPopData.R
+++ R/CleanUpPopData.R
@@ -7,7 +7,6 @@
 
 pop_data_output <- function(file, columns, sample_col, samples, NA_values){
   data <- read_csv(file = file, col_select = {{columns}}, na = NA_values)
-#  filtered_data <- filter(data, {{sample_col}} %in% samples)
     filtered_data <- data %>% 
     filter({{sample_col}} %in% samples) 
   write.table(filtered_data, "pop_output_file.txt", sep = "\t", row.names = FALSE)
