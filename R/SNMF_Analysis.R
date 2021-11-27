101dfd5d003a0e87d5c48f49bd1d970947752ace
soren <soren.johnson@selu.edu>
2021-11-27 19:02:20 +0000

    first draft of snmf analysis function

--- R/SNMF_Analysis.R
+++ R/SNMF_Analysis.R
@@ -1,4 +1,19 @@
 #Run a population structure analysis using a sparse non-negative matrix factorization algorithm
+#'
+#'
+#'
+#'
+#'
+#Need code below to install LEA
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#install.packages("BiocManager")
 
-#Expected inputs: Will be with the use of the LEA package. Input the population vcf file, range of K values to look at, the number of replicates
-#Expected outputs: the estimate of number of ancestral populations, a structure bar plot to look at the admxiture of samples with the plot aranged by the ancetral populations
+#BiocManager::install("LEA")
+
+op_K_snmf <- function(vcf_file, K, new_name, repetitions){
+  convert_output <- vcf2geno(vcf_file, new_name)
+  snmf_project = snmf(new_name, K = K, entropy = TRUE, repetitions = repetitions, project = "new")
+  cross_entropy_plot <- plot(snmf_project, cex = 1.2, col = 'lightblue', pch = 19)
+  summary(snmf_project)
+  return(cross_entropy_plot)
+}
