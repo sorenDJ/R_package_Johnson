861ae14e206b6af8b90090b45b1239e941e8f1bf
soren <soren.johnson@selu.edu>
2021-11-28 19:41:21 +0000

    Should hopefully be fully functional

--- R/SNMF_Analysis.R
+++ R/SNMF_Analysis.R
@@ -10,10 +10,12 @@
 
 #BiocManager::install("LEA")
 
-op_K_snmf <- function(vcf_file, K, new_name, repetitions){
+op_K_snmf <- function(vcf_file, K, new_name, repetitions, project_name){
   convert_output <- vcf2geno(vcf_file, new_name)
-  snmf_project = snmf(new_name, K = K, entropy = TRUE, repetitions = repetitions, project = "new")
-  cross_entropy_plot <- plot(snmf_project, cex = 1.2, col = 'lightblue', pch = 19)
-  summary(snmf_project)
+  assign(project_name, snmf(new_name, K = K, entropy = TRUE, repetitions = repetitions, project = "new"), envir = .GlobalEnv)
+  project_name <- snmf(new_name, K = K, entropy = TRUE, repetitions = repetitions, project = "new")
+  cross_entropy_plot <- plot(project_name, cex = 1.2, col = 'lightblue', pch = 19)
+  summary_1 <- summary(project_name)
+  return(summary_1)
   return(cross_entropy_plot)
 }
