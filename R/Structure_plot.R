971229caac0a62a5e773a4da4ec7e7d9c198e154
soren <soren.johnson@selu.edu>
2021-11-29 03:53:30 +0000

    First draft

--- R/Structure_plot.R
+++ R/Structure_plot.R
@@ -1,4 +1,14 @@
-#Create a structure/admixture plot that is ordered by inferred genetic cluster then longitude
+#Create a structure/admixture plot through LEA using snmf
+#'
+#'
+#'
+#'
+#'
 
-#Expected inputs: .Q file, text file with population data and GPS coordinates
-#Expected outputs: a structure bar plot that is ordered first by inferred genetic cluster then by longitude.
+
+snmf_structure_plot <- function(project_name, K, colors){
+  best <- which.min(cross.entropy(project_name, K = K))
+  structure_plot <- barchart(project_name, K = K, run = best, border = NA, space = 0, col = colors, xlab = "Individuals", ylab = "Ancestry Proportions", main = "Ancestry Matrix")
+  axis(1, at = 1:length(structure_plot$order), labels = structure_plot$order, las = 1, cex.axis = .4)
+  return(structure_plot)
+}
