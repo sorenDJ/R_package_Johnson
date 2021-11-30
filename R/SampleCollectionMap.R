73115926baf67d45025b151feea5f9dafe0acfbd
soren <soren.johnson@selu.edu>
2021-11-30 22:08:15 +0000

    deleting unused functions

--- R/SampleCollectionMap.R
+++
@@ -1,9 +0,0 @@
-#Create map of where samples were collected and have larger dots for areas that had more specimens collected
-
-#Expected inputs: a .csv file that contains sample longitudes and latitudes and well as other population data, give it names of columns to color the sample dots by
-#expected output: a map that contains the sample location and the more samples collected from that location the larger the symbol
-
-#'
-#'
-#'
-
