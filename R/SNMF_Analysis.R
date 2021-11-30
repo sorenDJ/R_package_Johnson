ff89c741a0ec1cac9fa37a14caec5569ad42e408
soren <soren.johnson@selu.edu>
2021-11-30 19:59:45 +0000

    Added params and test

--- R/SNMF_Analysis.R
+++ R/SNMF_Analysis.R
@@ -1,8 +1,12 @@
-#Run a population structure analysis using a sparse non-negative matrix factorization algorithm
+# Use a sparse non-negative matrix factorization algorithm to calculate optimal K and return snmf project
 #'
-#'
-#'
-#'
+#'@param vcf_file file path for the vcf file to be used for the analysis
+#'@param K a range of numbers to be used to check the number of ancestral populations
+#'@param new_name file path and name of the new converted .geno file
+#'@param repetitions number of times the cross-validation should be run
+#'@param project_name the name you want the snmf project to be called, needs to be in quotes
+#'@usage
+#' op_K_snmf(vcf_file = "/cloud/project/data/c_ven_phy_50_pop.vcf", K = 4:5, new_name = "/cloud/project/Output_files/test_2.geno", repetitions = 2, project_name = "snmf_test_2")
 #'
 #Need code below to install LEA
 #if (!requireNamespace("BiocManager", quietly = TRUE))
@@ -12,6 +16,9 @@
 
 op_K_snmf <- function(vcf_file, K, new_name, repetitions, project_name){
   convert_output <- vcf2geno(vcf_file, new_name)
+  if(is.numeric(repetitions) == FALSE){
+    return("ERROR-- repitions is not numeric")
+  }
   assign(project_name, snmf(new_name, K = K, entropy = TRUE, repetitions = repetitions, project = "new"), envir = .GlobalEnv)
   project_name <- snmf(new_name, K = K, entropy = TRUE, repetitions = repetitions, project = "new")
   cross_entropy_plot <- plot(project_name, cex = 1.2, col = 'lightblue', pch = 19)
