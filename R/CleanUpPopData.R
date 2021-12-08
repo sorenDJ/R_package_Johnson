b4ee7d9e0fe293c908c12a0395fc241bd4c296f4
soren <soren.johnson@selu.edu>
2021-12-08 20:12:11 +0000

    Update

--- R/CleanUpPopData.R
+++ R/CleanUpPopData.R
@@ -8,7 +8,7 @@
 #' @param NA_values values in the csv file that should be read as NA values
 #' @param output_file the file path for the output text file
 #' @usage
-#' pop_data_output(file = "/cloud/project/data/DNA_Samples_Data_Analysis.csv", columns = c(SJ_number, Species, sub_species), sample_col = SJ_number, samples = c("AR01", "AR02", "AR03"), NA_values = c("", "na"), output_file = "/cloud/project/Output_files/test_pop_data.txt")
+#' pop_data_output(file = "/cloud/project/data/DNA_Samples_Data_Analysis.csv", columns = c(SJ_number, Species, sub_species), sample_col = SJ_number, samples = c("AR01", "AR02", "AR03"), NA_values = c("", "na"), output_file = "/cloud/project/test_pop_data.txt")
 #' @export
 
 pop_data_output <- function(file, columns, sample_col, samples, NA_values, output_file){
